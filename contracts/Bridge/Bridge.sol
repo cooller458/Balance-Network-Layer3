1858df40c53574a133130f76e103e529f4959ceb
Cooller458 <omrelibol4@gmail.com>
2023-03-24 17:47:41 +0300

    add : Bridge.sol

--- contracts/Bridge/Bridge.sol
+++ contracts/Bridge/Bridge.sol
@@ -2,7 +2,254 @@
 
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts-upgredeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgredeable/utils/AddresUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+
+import {
+    NotContract,
+    NotRollupOrOwner,
+    NotDelayedInbox,
+    NotSequencerInbox,
+    NotOutbox,
+    InvalidOutboxSet,
+    BadSequencerMessageNumber
+} from "../libraries/Error.sol";
+import "./IBridge.sol";
+import "./Messages.sol";
+import "../libraries/DelegateCallAware.sol";
+
+import {L1MessageType_batchPostingReport} from "../libraries/MessageTypes.sol";
+
+contract Bridge is Initializable, DelegateCallAware, IBridge {
+    using AddressUpgradeable for address;
+
+    struct InOutInfo {
+        uint256 index;
+        bool allowed;
+    }
+    mapping (address => InOutInfo) public allowedDelayedInboxedMap;
+    mapping (address => InOutInfo) public allowedOutboxesMap;
+
+    address[] public allowedDelayedInboxList;
+    address[] public allowedOutboxList;
+
+    address internal _activeOutbox;
+
+    bytes32[] public delayedInboxAccs;
+
+    bytes32[] public  sequencerInboxAccs;
+
+    IOwnable public rollup;
+
+    address public sequencer;
+
+    uint256 public ovveride sequencerReportedSubMessageCount;
+
+    address internal constant EMPTY_ACTIVEOUTBOX = address(type(uint160).max);
+
+    function initialize(IOwnable rollup_) external initializer onlyDelegated {
+        _activeOutbox = EMPTY_ACTIVEOUTBOX;
+        rollup = rollup_;
+    }
+
+    modifier onlyRollupOrOWner() {
+
+        if (msg.sender != address(rollup)) {
+            address rollupOwner == rollup.owner();
+            if (msg.sender != rollupOwner) {
+                revert NotRollupOrOwner(msg.sender , address(rollup), rollupOwner);
+            }
+        }
+        _;
+    }
+
+    function activeOutbox() public view returns(address) {
+        address outbox = _activeOutbox;
+
+        if (outbox  = EMPTY_ACTIVEOUTBOX) return address(0);
+        return outbox;
+
+    }
+    function allowedDelayedInboxes(address inbox) external view returns (bool) {
+        return allowedDelayedInboxedMap[inbox].allowed;
+    }
+    function allowedOuboxes(address outbox) external view returns(bool) {
+        return allowedOutboxesMap[outbox].allowed;
+    }
+
+    modifier onlySequencerInbox() {
+        if(msg.sender != sequencerInbox) revert NotSequencerInbox(msg.sender);
+    }
+    function enqueueSequencerMessage(
+        bytes32 dataHash,
+        uint256 afterDelayedMessagesRead,
+        uint256 prevMessageCount,
+        uint256 newMessageCount
+    ) external onlySequencerInbox returns (
+        uint256 seqMessageIndex,
+        bytes32 beforeAcc,
+        bytes32 delayedAcc,
+        bytes32 acc
+    ) {
+         if (
+            sequencerReportedSubMessageCount != prevMessageCount &&
+            prevMessageCount != 0 &&
+            sequencerReportedSubMessageCount != 0
+        ) {
+            revert BadSequencerMessageNumber(sequencerReportedSubMessageCount, prevMessageCount);
+        }
+        sequencerReportedSubMessageCount = newMessageCount;
+        seqMessageIndex = sequencerInboxAccs.length;
+        if (sequencerInboxAccs.length > 0) {
+            beforeAcc = sequencerInboxAccs[sequencerInboxAccs.length - 1];
+        }
+        if (afterDelayedMessagesRead > 0) {
+            delayedAcc = delayedInboxAccs[afterDelayedMessagesRead - 1];
+        }
+        acc = keccak256(abi.encodePacked(beforeAcc, dataHash, delayedAcc));
+        sequencerInboxAccs.push(acc);
+    }
+    function submitBatchSpendingReport(address sender, bytes32 messageDataHash)
+        external
+        onlySequencerInbox
+        returns (uint256)
+    {
+        return
+            addMessageToDelayedAccumulator(
+                L1MessageType_batchPostingReport,
+                sender,
+                uint64(block.number),
+                uint64(block.timestamp), // solhint-disable-line not-rely-on-time,
+                block.basefee,
+                messageDataHash
+            );
+    }
+    function enqueueDelayedMessage(
+        uint8 kind,
+        address sender,
+        bytes32 messageDataHash
+    ) external payable returns (uint256) {
+        if (!allowedDelayedInboxesMap[msg.sender].allowed) revert NotDelayedInbox(msg.sender);
+        return
+            addMessageToDelayedAccumulator(
+                kind,
+                sender,
+                uint64(block.number),
+                uint64(block.timestamp), // solhint-disable-line not-rely-on-time
+                block.basefee,
+                messageDataHash
+            );
+    }
+
+    function addMessageToDelayedAccumulator(
+        uint8 kind,
+        address sender,
+        uint64 blockNumber,
+        uint64 blockTimestamp,
+        uint256 baseFeeL1,
+        bytes32 messageDataHash
+    ) internal returns (uint256) {
+        uint256 count = delayedInboxAccs.length;
+        bytes32 messageHash = Messages.messageHash(
+            kind,
+            sender,
+            blockNumber,
+            blockTimestamp,
+            count,
+            baseFeeL1,
+            messageDataHash
+        );
+        bytes32 prevAcc = 0;
+        if (count > 0) {
+            prevAcc = delayedInboxAccs[count - 1];
+        }
+        delayedInboxAccs.push(Messages.accumulateInboxMessage(prevAcc, messageHash));
+        emit MessageDelivered(
+            count,
+            prevAcc,
+            msg.sender,
+            kind,
+            sender,
+            messageDataHash,
+            baseFeeL1,
+            blockTimestamp
+        );
+        return count;
+    }
+    function executeCall(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external returns (bool success, bytes memory returnData) {
+        if (!allowedOutboxesMap[msg.sender].allowed) revert NotOutbox(msg.sender);
+        if (data.length > 0 && !to.isContract()) revert NotContract(to);
+        address prevOutbox = _activeOutbox;
+        _activeOutbox = msg.sender;
+        (success, returnData) = to.call{value: value}(data);
+        _activeOutbox = prevOutbox;
+        emit BridgeCallTriggered(msg.sender, to, value, data);
+    }
+
+    function setSequencerInbox(address _sequencerInbox) external onlyRollupOrOwner {
+        sequencerInbox = _sequencerInbox;
+        emit SequencerInboxUpdated(_sequencerInbox);
+    }
+
+    function setDelayedInbox(address inbox, bool enabled) external onlyRollupOrOwner {
+        InOutInfo storage info = allowedDelayedInboxesMap[inbox];
+        bool alreadyEnabled = info.allowed;
+        emit InboxToggle(inbox, enabled);
+        if ((alreadyEnabled && enabled) || (!alreadyEnabled && !enabled)) {
+            return;
+        }
+        if (enabled) {
+            allowedDelayedInboxesMap[inbox] = InOutInfo(allowedDelayedInboxList.length, true);
+            allowedDelayedInboxList.push(inbox);
+        } else {
+            allowedDelayedInboxList[info.index] = allowedDelayedInboxList[
+                allowedDelayedInboxList.length - 1
+            ];
+            allowedDelayedInboxesMap[allowedDelayedInboxList[info.index]].index = info.index;
+            allowedDelayedInboxList.pop();
+            delete allowedDelayedInboxesMap[inbox];
+        }
+    }
+
+    function setOutbox(address outbox, bool enabled) external onlyRollupOrOwner {
+        if (outbox == EMPTY_ACTIVEOUTBOX) revert InvalidOutboxSet(outbox);
+
+        InOutInfo storage info = allowedOutboxesMap[outbox];
+        bool alreadyEnabled = info.allowed;
+        emit OutboxToggle(outbox, enabled);
+        if ((alreadyEnabled && enabled) || (!alreadyEnabled && !enabled)) {
+            return;
+        }
+        if (enabled) {
+            allowedOutboxesMap[outbox] = InOutInfo(allowedOutboxList.length, true);
+            allowedOutboxList.push(outbox);
+        } else {
+            allowedOutboxList[info.index] = allowedOutboxList[allowedOutboxList.length - 1];
+            allowedOutboxesMap[allowedOutboxList[info.index]].index = info.index;
+            allowedOutboxList.pop();
+            delete allowedOutboxesMap[outbox];
+        }
+    }
+
+    function setSequencerReportedSubMessageCount(uint256 newMsgCount) external onlyRollupOrOwner {
+        sequencerReportedSubMessageCount = newMsgCount;
+    }
+
+    function delayedMessageCount() external view override returns (uint256) {
+        return delayedInboxAccs.length;
+    }
+
+    function sequencerMessageCount() external view returns (uint256) {
+        return sequencerInboxAccs.length;
+    }
+
+    function acceptFundsFromOldBridge() external payable {}
+
+
+}
 
 
